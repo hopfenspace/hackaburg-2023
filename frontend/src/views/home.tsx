9ea63ce551c1ddec34c2b7cf95f0fbefd2265848
gammelalf <gammelalf@hopfen.space>
2023-05-26 08:53:47 +0200

    Styled <Home /> a bit

--- frontend/src/views/home.tsx
+++ frontend/src/views/home.tsx
@@ -1,15 +1,40 @@
-import Input from "../components/input";
+import Input, { InputProps } from "../components/input";
 import LOCATION_SVG from "../assets/location.svg";
 import React from "react";
+import "../style/home.css";
 
 type HomeProps = {};
 export default function Home(props: HomeProps) {
     const [search, setSearch] = React.useState("");
     const [postalCode, setPostalCode] = React.useState("");
     return (
-        <div>
-            <Input placeholder="Was möchtest du bestellen?" value={search} onChange={setSearch} />
+        <div className="home">
+            <SearchBar
+                placeholder="Was möchtest du bestellen?"
+                value={search}
+                onChange={setSearch}
+                suggestions={["foo", "bar", "baz"]}
+            />
             <PostalInput value={postalCode} onChange={setPostalCode} />
+        </div>
+    );
+}
+
+type SearchBarProps = InputProps & {
+    suggestions?: Array<string>;
+};
+function SearchBar(props: SearchBarProps) {
+    const { suggestions, ...inputProps } = props;
+    return (
+        <div className="search-bar">
+            <Input {...inputProps} />
+            {suggestions && suggestions.length > 0 && (
+                <div>
+                    {suggestions.map((suggestion) => (
+                        <div>{suggestion}</div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
@@ -20,10 +45,11 @@ type PostalInputProps = {
 };
 function PostalInput(props: PostalInputProps) {
     return (
-        <div>
+        <div className="postal-input">
             <Input value={props.value} onChange={props.onChange} placeholder="Postleitzahl" />
             <button
                 type="button"
+                className="button"
                 onClick={() => {
                     window.navigator.geolocation.getCurrentPosition(() => props.onChange("93053"));
                 }}
